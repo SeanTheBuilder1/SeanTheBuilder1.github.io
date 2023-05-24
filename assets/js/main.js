65b88976e4f063a9cc442032e59842b01d24187e
Sean Patrick <68685725+SeanTheBuilder1@users.noreply.github.com>
2023-05-24 10:02:50 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,9 @@ function foo(){
     alert('I got clicked')
 }
 
-document.getElemenById('fuck').addEventListener('click', foo)
+function bar(){
+    alert('I got clicked 2')
+}
+
+document.getElementById("btn1").addEventListener("click", foo)
+document.getElementById("btn2").addEventListener("click", foo)
