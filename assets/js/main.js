6382df34178814fb89b155b5290142cee075aead
Sean Patrick <68685725+SeanTheBuilder1@users.noreply.github.com>
2023-05-28 11:39:43 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,50 @@
 open = false
 slidemenu = document.getElementById("slidemenu")
+var checkbox = document.querySelector("input[name=darkmode]"); 
+body = document.body
+
+console.log(localStorage.getItem('display_mode'))
+display_mode = localStorage.getItem('display_mode')
+
+
+
+if(display_mode == 'dark'){
+    checkbox.checked = true
+    console.log('init dark mode')
+    body.classList.add(display_mode)
+}
+else if(display_mode == 'light'){
+    console.log('init light')
+    body.classList.add(display_mode)
+}
+else{
+    localStorage.setItem('display_mode', 'dark')
+    body.classList.add('dark')
+}
+
+
+checkbox.addEventListener('change', function(){
+    if(this.checked){ 
+        darken() 
+        localStorage.setItem('display_mode', 'dark')
+    } 
+    else{
+        lighten() 
+        localStorage.setItem('display_mode', 'light')
+    }
+});
+
+function darken(){
+    console.log("setting dark mode")
+    body.classList.replace('light', 'dark')
+    
+}
+
+function lighten(){
+    console.log("setting light mode")
+    body.classList.replace('dark', 'light')
+}
+
 
 function foo(){
     if(open){
@@ -18,3 +63,22 @@ function bar(){
 
 document.getElementById("btn1").addEventListener("click", foo)
 //document.getElementById("btn2").addEventListener("click", bar)
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach(function(lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
+});
